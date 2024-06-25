62b4b350dd03478aab396ab74d062805861213e2
Juan Ignacio Causse <jcausse@itba.edu.ar>
2024-06-25 03:49:00 -0300

    Added manager parser files. Makefile changes.

---
+++ build_manager.sh
@@ -0,0 +1,27 @@
+exec_name=manager
+target_exec_name=manager
+could_build=0
+
+if [ $# -ge 1 ]; then
+    target_exec_name=$1
+fi
+
+cd ./src/manager
+make clean
+CC=gcc make
+
+if [ $? -eq 0 ]; then
+    mv $exec_name ../../$target_exec_name
+    could_build=1
+fi
+
+make clean
+
+if [ $could_build -eq 1 ]; then
+    echo ""
+    echo "********************"
+    echo "* Build successful *"
+    echo "********************"
+    echo ""
+fi
+
